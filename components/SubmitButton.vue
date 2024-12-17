9da836119063df45feb40010799b405a8a4afa3f
Tigran <harutyunyan.tigran1975@gmail.com>
2024-12-17 10:18:59 +0400

    created SubmitButton component

---
+++ components/SubmitButton.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import { Loader2 } from "lucide-vue-next";
+
+interface iAppProps {
+  text: string;
+  variant?:
+    | "default"
+    | "destructive"
+    | "outline"
+    | "secondary"
+    | "ghost"
+    | "link"
+    | null
+    | undefined;
+
+  className?: string;
+  pending?: boolean;
+}
+
+defineProps<iAppProps>();
+</script>
+
+<template>
+  <Button
+    v-if="pending"
+    disabled
+    variant="outline"
+    :class="cn('w-fit', className)"
+    type="submit"
+  >
+    <Loader2 class="size-4 mr-2 animate-spin" /> Please wait
+  </Button>
+
+  <Button
+    v-else
+    :variant="variant"
+    type="submit"
+    :class="cn('w-fit', className)"
+  >
+    {{ text }}
+  </Button>
+</template>
