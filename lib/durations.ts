98cb51e49e393870aee8033e9603e5278b9e0a02
Tigran <harutyunyan.tigran1975@gmail.com>
2024-12-17 18:49:57 +0400

    created data

---
+++ lib/durations.ts
@@ -0,0 +1,18 @@
+export const durations = [
+    {
+        duration: 15,
+        text: "15 Min",
+    },
+    {
+        duration: 30,
+        text: "30 Min",
+    },
+    {
+        duration: 45,
+        text: "45 Min",
+    },
+    {
+        duration: 60,
+        text: "1 Hour",
+    },
+]
---
+++ lib/platforms.ts
@@ -0,0 +1,14 @@
+export const platforms = [
+    {
+        id: "zoom",
+        text: "Zoom Meeting",
+    },
+    {
+        id: "google_meet",
+        text: "Google Meet",
+    },
+    {
+        id: "microsoft_teams",
+        text: "Microsoft Teams",
+    },
+]
