d09318a1cc8c7a3505d8dc825a1fd54dba909410
Tigran <harutyunyan.tigran1975@gmail.com>
2025-01-12 14:41:16 +0400

    created home page

---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import Hero from "@/components/landing-page/Hero.vue";
+import Logos from "@/components/landing-page/Logos.vue";
+import Features from "@/components/landing-page/Features.vue";
+import Testimonial from "@/components/landing-page/Testimonial.vue";
+import CTA from "@/components/landing-page/CTA.vue";
+import Navbar from "@/components/landing-page/Navbar.vue";
+
+definePageMeta({
+  middleware: ["auth"],
+});
+</script>
+<template>
+  <NuxtLayout>
+    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <Navbar />
+      <Hero />
+      <Logos />
+      <Features />
+      <Testimonial />
+      <CTA />
+    </div>
+  </NuxtLayout>
+</template>
