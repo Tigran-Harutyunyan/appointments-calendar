636089af6e0540d0cfc7e0b92d72aabd3f8f354f
Tigran <harutyunyan.tigran1975@gmail.com>
2025-01-12 14:55:05 +0400

    app file

---
+++ app.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { UserContextProvider } from "@nuxtbase/auth-ui-vue";
+import { Toaster } from "vue-sonner";
+
+const supabaseClient = useSupabaseClient();
+</script>
+<template>
+  <UserContextProvider :supabaseClient="supabaseClient">
+    <NuxtLoadingIndicator />
+
+    <NuxtLayout>
+      <NuxtPage />
+      <ClientOnly>
+        <Toaster
+          position="bottom-right"
+          :rich-colors="true"
+          :close-button="true"
+        />
+      </ClientOnly>
+    </NuxtLayout>
+  </UserContextProvider>
+</template>
