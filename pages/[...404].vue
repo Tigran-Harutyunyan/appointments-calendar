9dd1e25ba7d16c7f3efcb01979c4c967bf300f92
Tigran <harutyunyan.tigran1975@gmail.com>
2025-01-12 14:42:47 +0400

    404 page

---
+++ pages/[...404].vue
@@ -0,0 +1,24 @@
+<script setup>
+import { Button } from "~/components/ui/button";
+
+useHead({
+  title: "Scheduler | 404",
+});
+</script>
+
+<template>
+  <div class="min-h-screen">
+    <div
+      class="w-full lmd:w-1/2 relative pb-12 lg:pb-0 flex items-center justify-center h-screen"
+    >
+      <div class="relative flex items-center flex-col space-y-8">
+        <div class="mb-12 mx-auto">
+          <img src="../assets/images/404.svg" class="mx-auto w-[300px]" />
+        </div>
+        <NuxtLink to="/" class="mx-auto">
+          <Button>GO HOME</Button>
+        </NuxtLink>
+      </div>
+    </div>
+  </div>
+</template>
