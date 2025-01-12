8fca6cda58a34c044288178befade2e4b8051520
Tigran <harutyunyan.tigran1975@gmail.com>
2025-01-12 14:42:56 +0400

    created icons

---
+++ components/icons/ColorModeIcon.vue
@@ -0,0 +1,16 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 19.82 29.21"
+    class="text-primary-icon h-5 fill-current cursor-pointer"
+  >
+    <g id="Layer_2" data-name="Layer 2">
+      <g id="Capa_1" data-name="Capa 1">
+        <path
+          class="cls-1"
+          d="M10.43,14.61A14.63,14.63,0,0,1,19.82,1a14.31,14.31,0,0,0-5.21-1,14.61,14.61,0,1,0,5.21,28.25A14.65,14.65,0,0,1,10.43,14.61Z"
+        />
+      </g>
+    </g>
+  </svg>
+</template>
