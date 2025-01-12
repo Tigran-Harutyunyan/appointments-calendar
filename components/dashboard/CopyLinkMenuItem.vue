4273815f61e7f1e65e6b246c8b3d50ddce8d3f9e
Tigran <harutyunyan.tigran1975@gmail.com>
2025-01-12 14:43:50 +0400

    created a component to copy to clipboard

---
+++ components/dashboard/CopyLinkMenuItem.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { DropdownMenuItem } from "@/components/ui/dropdown-menu";
+import { Link2 } from "lucide-vue-next";
+import { toast } from "vue-sonner";
+
+interface CopyLinkMenuItemProps {
+  meetingUrl: string;
+}
+
+const props = defineProps<CopyLinkMenuItemProps>();
+
+const handleCopy = async () => {
+  try {
+    await navigator.clipboard.writeText(props.meetingUrl);
+    toast.success("URL copied to clipboard");
+  } catch (err) {
+    toast.error("Failed to copy URL");
+  }
+};
+</script>
+
+<template>
+  <DropdownMenuItem @select="handleCopy">
+    <Link2 class="mr-2 h-4 w-4" />
+    <span>Copy</span>
+  </DropdownMenuItem>
+</template>
