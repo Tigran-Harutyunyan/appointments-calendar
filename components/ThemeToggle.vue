3b843d90e964897d876b621b4cd703c3949e527d
Tigran <harutyunyan.tigran1975@gmail.com>
2025-01-12 14:38:29 +0400

    created Themetoggle component

---
+++ components/ThemeToggle.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { SunIcon } from "lucide-vue-next";
+import { useDark, useToggle } from "@vueuse/core";
+
+const isDark = useDark({
+  selector: "html",
+  valueDark: "dark",
+  valueLight: "light",
+});
+
+const toggleDark = useToggle(isDark);
+</script>
+
+<template>
+  <SunIcon @click="toggleDark()" class="cursor-pointer" />
+</template>
