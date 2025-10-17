2c4d845ef9eae3b0173189d2fb83974bf74f2262
Tigran <harutyunyan.tigran1975@gmail.com>
2025-10-17 13:17:22 +0400

    config changes

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,45 +3,46 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   ssr: false,
   modules: [
-    '@nuxtjs/tailwindcss',
-    'shadcn-nuxt',
-    '@vueuse/nuxt',
-    '@nuxt/image',
-    '@pinia/nuxt',
-    '@vee-validate/nuxt',
-    '@nuxtjs/supabase',
-    '@uploadthing/nuxt',
+    "@nuxtjs/tailwindcss",
+    "shadcn-nuxt",
+    "@vueuse/nuxt",
+    "@nuxt/image",
+    "@pinia/nuxt",
+    "@vee-validate/nuxt",
+    "@nuxtjs/supabase",
+    "@uploadthing/nuxt",
   ],
   alias: {
-    ".prisma/client/index-browser": "./node_modules/.prisma/client/index-browser.js"
+    ".prisma/client/index-browser":
+      "./node_modules/.prisma/client/index-browser.js",
   },
   supabase: {
     // Options
     url: process.env.SUPABASE_URL,
     key: process.env.SUPABASE_KEY,
     redirectOptions: {
-      login: '/',
-      callback: '/',
+      login: "/",
+      callback: "/",
       include: undefined,
       exclude: [],
-      cookieRedirect: false,
-    }
+      cookieRedirect: true,
+    },
   },
   shadcn: {
     /**
      * Prefix for all the imported component
      */
-    prefix: '',
+    prefix: "",
     /**
      * Directory that the component lives in.
      * @default "./components/ui"
      */
-    componentDir: './components/ui'
+    componentDir: "./components/ui",
   },
   runtimeConfig: {
     public: {
       appUrl: process.env.NUXT_PUBLIC_APP_URL,
     },
   },
-  compatibilityDate: '2024-11-10',
-})
+  compatibilityDate: "2024-11-10",
+});
